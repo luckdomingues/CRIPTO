0c839d4d04837c6fed4ec3c3495f6aa24c09c37e
SiRi <luciano.domingues@gmail.com>
2022-07-05 14:04:07 -0300

    Create cesar.py
    script inicial

---
+++ cesar.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+
+from string import lowercase as lc
+
+file = open(sys.argv[1],'r').read().lower()
+
+for key in xrange(1,26):
+ result = ''
+ print 'Key:', key
+ for lt in file:
+  if lt in lc:
+   idx = lc.find(lt)
+   idx = (idx - key) % 26
+   result += lc[idx]
+  else:
+   result += lt
+ print result
